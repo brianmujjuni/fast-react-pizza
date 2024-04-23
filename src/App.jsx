e49ab004099c235c6e266096a383a86151fc325e
brianmujjuni <brianmujjuni@engineer.com>
2024-04-23 16:19:39 +0300

    create Home and Menu routes in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./global/Home";
+import Menu from "./features/menu/Menu";
+createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+createBrowserRouter;
 export default function App() {
   return <div>Hello Vite</div>;
 }
