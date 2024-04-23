5f97387c15279f79935be1fff42c8f5e5d30f8d0
brianmujjuni <brianmujjuni@engineer.com>
2024-04-23 16:39:50 +0300

    create all pending routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./global/Home";
 import Menu from "./features/menu/Menu";
-createBrowserRouter([
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
@@ -10,8 +14,11 @@ createBrowserRouter([
     path: "/menu",
     element: <Menu />,
   },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
 ]);
 createBrowserRouter;
 export default function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
