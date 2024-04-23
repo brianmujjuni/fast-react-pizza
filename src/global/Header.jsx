75290a17dc9941449a2fceb1c80115a0932c88e5
brianmujjuni <brianmujjuni@engineer.com>
2024-04-23 17:17:48 +0300

    create applayout

---
+++ src/global/Header.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">Automex Fast Foodss</Link>
+      <p>Automex</p>
+    </header>
+  );
+}
