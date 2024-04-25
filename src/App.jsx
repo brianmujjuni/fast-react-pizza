7f2e1b01b0a2d4d1bffcfeee35fcfdd7becad99d
brianmujjuni <brianmujjuni@engineer.com>
2024-04-25 09:09:18 +0300

    implement error element in createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
