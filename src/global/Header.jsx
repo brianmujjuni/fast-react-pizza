09b59fb46edcb7a5a0b43baeb0a39492f079d9e4
brianmujjuni <brianmujjuni@engineer.com>
2024-04-26 12:48:51 +0300

    add tailwind text color and flex box

--- src/global/Header.jsx
+++ src/global/Header.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Automex Fast Foodss</Link>
+    <header className=" flex items-center justify-between bg-yellow-500 p-2 uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Automex Fast Foodss
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
