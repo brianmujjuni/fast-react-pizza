76e782ebdbd9a4f315e3ddb41ae394e544ebd18f
brianmujjuni <brianmujjuni@engineer.com>
2024-04-27 14:33:04 +0300

    create button component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -6,6 +6,7 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../global/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -85,12 +86,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
-            {isSubmitting ? "Placing order" : "Order now"}
-          </button>
+          <Button disabled={isSubmitting}>{isSubmitting ? "Placing order" : "Order now"}</Button>
         </div>
       </Form>
     </div>
---
+++ src/global/Button.jsx
@@ -0,0 +1,15 @@
+export default function Button({ children, disabled }) {
+  return (
+    <button
+      disabled={disabled}
+      className="bg-yellow-400 uppercase
+       font-semibold tracking-wide text-stone-800 
+       py-3 px-4 inline-block rounded-full hover:bg-yellow-300
+        transition-colors duration-300 focus:outline-none focus:ring
+         focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2
+          disabled:cursor-not-allowed"
+    >
+      {children}
+    </button>
+  );
+}
