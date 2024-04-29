8e92c476cf9afe3e6257a85cafed1bf44f137d3f
brianmujjuni <brianmujjuni@engineer.com>
2024-04-29 09:53:46 +0300

    change default font

--- src/global/Header.jsx
+++ src/global/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
 export default function Header() {
   return (
-    <header className=" flex items-center justify-between bg-yellow-500 p-2 uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
+    <header className=" flex items-center justify-between bg-yellow-500 p-2 uppercase px-4 py-3 border-b border-stone-200 sm:px-6 ">
       <Link to="/" className="tracking-widest">
         Automex Fast Foodss
       </Link>
