5abdab79cc78f8a7dc35744662df16da37eb79ee
brianmujjuni <brianmujjuni@engineer.com>
2024-04-30 20:31:05 +0300

    style orderItem component

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -42,9 +42,9 @@ function Order() {
         </p>
         <p className="text-xs text-stone-500">(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
-            <ul className="divide-y divide-stone-200 border-b border-top">
+        <ul className="divide-y divide-stone-200 border-b border-top">
               {cart.map(item =><OrderItem item={item} key={item.id}/> )}
-            </ul>
+        </ul>
       <div className="space-y-2 bg-stone-200 py-5 px-6">
         <p className="text-sm font-medium text-stone-600">Price pizza: {formatCurrency(orderPrice)}</p>
         {priority && <p className="text-sm font-medium text-stone-600">Price priority: {formatCurrency(priorityPrice)}</p>}
