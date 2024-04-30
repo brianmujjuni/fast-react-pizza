127962930b4512288f663db8de3c2ea1c4a94ed9
brianmujjuni <brianmujjuni@engineer.com>
2024-04-30 19:46:02 +0300

    style order component

--- src/global/Button.jsx
+++ src/global/Button.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 export default function Button({ children, disabled, to,type }) {
   
    
-    const base = "bg-yellow-400 uppercase  font-semibold tracking-wide text-stone-800  inline-block hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed ";
+    const base = "bg-yellow-400 text-sm uppercase  font-semibold tracking-wide text-stone-800  inline-block hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed ";
    
     const style = {
       primary: base + 'px-4 py-3 md:px-6 md:py-4 rounded-full',
       small: base + 'px-4 py-2 md:px-5 md:py-2.5 rounded-full text-xs',
-      secondary: "border-2 border-stone-300 px-4 py-3 md:px-6 md:py-4 rounded-full uppercase  font-semibold tracking-wide text-stone-800  inline-block hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed "
+      secondary: "text-sm border-2 border-stone-300 px-4 py-3 md:px-6 md:py-4 rounded-full uppercase  font-semibold tracking-wide text-stone-800  inline-block hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed "
     }
 
 
