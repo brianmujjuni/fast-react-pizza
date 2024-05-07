413b69da42ef2a23ce7ec14ccf5c54d9fc4dd830
brianmujjuni <brianmujjuni@engineer.com>
2024-05-07 14:56:41 +0300

    add functionality to cart actions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,16 @@ import { createSlice } from "@reduxjs/toolkit"
 
 
 const initialState={
-    cart: [],
+    // cart: [],
+    cart: [
+        {
+            pizzaId: 12,
+            name: "Mediterranean",
+            quantity: 2,
+            unitPrice:16,
+            totalPrice: 32,
+        }
+    ]
 }
 
 const cartSlice = createSlice({
@@ -10,13 +19,24 @@ const cartSlice = createSlice({
     initialState,
     reducers:{
         addItem(state,action){
-
+            state.cart.push(action.payload)
         },
-        deleteItem(state,action){},
+        deleteItem(state,action){
+            state.cart = state.cart.filter(item =>item.pizzaId !== action.payload)
+        },
         increaseItemQuantity(state,action){
+            const item = state.cart.find(item =>item.pizzaId === action.payload)
+            item.quantity++
+            item.totalPrice = item.quantity * item.unitPrice
 
         },
-        decreaseItemQuantity(state,action){},
-        clearCart(state,action){}
+        decreaseItemQuantity(state,action){
+            const item = state.cart.find(item =>item.pizzaId === action.payload)
+            item.quantity--
+            item.totalPrice = item.quantity * item.unitPrice
+        },
+        clearCart(state){
+            state.cart = []
+        }
     }
 })
