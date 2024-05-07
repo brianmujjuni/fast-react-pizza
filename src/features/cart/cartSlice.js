2b055532f2a359c5f5b6399cc28be75cffb5e19c
brianmujjuni <brianmujjuni@engineer.com>
2024-05-07 13:05:44 +0300

    create cart slice and reducer actions

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState={
+    cart: [],
+}
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers:{
+        addItem(state,action){
+
+        },
+        deleteItem(state,action){},
+        increaseItemQuantity(state,action){
+
+        },
+        decreaseItemQuantity(state,action){},
+        clearCart(state,action){}
+    }
+})
