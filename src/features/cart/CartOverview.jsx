8fee7b0877bad235849b00b319bef88504304a04
brianmujjuni <brianmujjuni@engineer.com>
2024-05-07 19:35:54 +0300

    condition cartOverview component display

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -5,6 +5,8 @@ import { formatCurrency } from "../../utilities/helpers";
 function CartOverview() {
  const totalCartQuantity =  useSelector(getTotalCartQuantity)
  const totalCartPrice = useSelector(getTotalCartPrice)
+
+ if(!totalCartQuantity) return null;
   return (
     <div className="bg-stone-800 text-stone-200 uppercase px-4 py-4 sm:px-6 text-sm md:text-base flex items-center justify-between ">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
