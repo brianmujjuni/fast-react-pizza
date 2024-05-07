f362e6d6b5309ef7a18198652d799ec13d397fa7
brianmujjuni <brianmujjuni@engineer.com>
2024-05-07 10:52:25 +0300

    create user store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "./features/user/userSlice"
+const store = configureStore({
+    reducer:{
+        user: UserReducer,
+    }
+})
+
+export default store
