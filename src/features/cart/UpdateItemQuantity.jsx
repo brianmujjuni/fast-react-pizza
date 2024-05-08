5b72f1b647f2e6a38526457e9cb82a6037ef0c94
brianmujjuni <brianmujjuni@engineer.com>
2024-05-08 17:53:30 +0300

    prevent decresing 0 items in cart

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Button from '../../global/Button'
+import { useDispatch } from 'react-redux'
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice'
 
 export default function UpdateItemQuantity({pizzaId}) {
+    const dispatch = useDispatch()
   return (
-    <div>
-        <Button type="round">-</Button>
-        <Button type="round">+</Button>
+    <div className='flex gap-1 items-center md:gap-3'>
+        <Button type="round" onClick={()=>dispatch(decreaseItemQuantity(pizzaId))}>-</Button>
+        <Button type="round" onClick={()=>dispatch(increaseItemQuantity(pizzaId))}>+</Button>
     </div>
   )
 }
