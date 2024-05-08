7e9f01aed11d32fb914d30703618f0fe7ae9b440
brianmujjuni <brianmujjuni@engineer.com>
2024-05-08 17:45:11 +0300

    create round button type css

--- src/global/Button.jsx
+++ src/global/Button.jsx
@@ -7,7 +7,8 @@ export default function Button({ children, disabled, to,type,onClick }) {
     const style = {
       primary: base + 'px-4 py-3 md:px-6 md:py-4 rounded-full',
       small: base + 'px-4 py-2 md:px-5 md:py-2.5 rounded-full text-xs',
-      secondary: "text-sm border-2 border-stone-300 px-4 py-3 md:px-6 md:py-4 rounded-full uppercase  font-semibold tracking-wide text-stone-800  inline-block hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed "
+      secondary: "text-sm border-2 border-stone-300 px-4 py-3 md:px-6 md:py-4 rounded-full uppercase  font-semibold tracking-wide text-stone-800  inline-block hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed ",
+      round: base + 'px-2.5 py-1 md:px-3.5 md:py-2 rounded-full text-sm',
     }
 
 
