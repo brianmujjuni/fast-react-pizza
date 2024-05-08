366408c4b1b5dd37c5918b3960d116e327b40745
brianmujjuni <brianmujjuni@engineer.com>
2024-05-08 17:42:35 +0300

    add increase and decrease buttons

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '../../global/Button'
 
-export default function UpdateItemQuantity() {
+export default function UpdateItemQuantity({pizzaId}) {
   return (
     <div>
         <Button type="round">-</Button>
