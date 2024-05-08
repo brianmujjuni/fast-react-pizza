fe0c7258f28bc85a518f3c645b1d24ef26fd9832
brianmujjuni <brianmujjuni@engineer.com>
2024-05-08 18:08:20 +0300

    add updateQuantity component to menuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import Button from "../../global/Button"
 import { useDispatch, useSelector } from "react-redux";
 import {addItem, getCurrentQuantityById} from "../cart/cartSlice"
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
 
@@ -34,7 +35,10 @@ function MenuItem({ pizza }) {
         <div className="mt-auto flex items-center justify-between pt-0.5 ">
        
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
-          {isInCart && <DeleteItem pizzaId={id}/>}
+          {isInCart &&<div className="flex items-center gap-3 sm:gap-8">
+            <UpdateItemQuantity pizzaId={id} currentQuantity={currentQuantity}/>
+            <DeleteItem pizzaId={id}/>
+          </div> }
           {!soldOut && !isInCart && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
         </div>
       </div>
