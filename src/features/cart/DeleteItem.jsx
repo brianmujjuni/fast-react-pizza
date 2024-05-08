e28d3656a8629e68e110412fbf01b26a24fb5883
brianmujjuni <brianmujjuni@engineer.com>
2024-05-08 10:04:08 +0300

    create deleteItem component & add delete action to cartItem component

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,11 @@
+
+import { useDispatch } from 'react-redux'
+import Button from '../../global/Button'
+import { deleteItem } from './cartSlice'
+
+export default function DeleteItem({pizzaId}) {
+    const dispatch = useDispatch()
+  return (
+    <Button type="small" onClick={()=>dispatch(deleteItem(pizzaId))}>Delete</Button>
+  )
+}
